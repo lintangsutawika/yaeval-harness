13c77c2c276aa91e4d150b59abed8b394709a370
lintangsutawika <lintang@eleuther.ai>
2024-10-18 17:47:02 -0400

    custom interface

---
+++ interface.py
@@ -0,0 +1,50 @@
+import torch
+import transformers
+
+import pal
+
+
+class LlamaProgramInterface(pal.interface.ProgramChatInterface):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.model = transformers.pipeline(
+            "text-generation",
+            model=self.model,
+            model_kwargs={"torch_dtype": torch.bfloat16},
+            device_map="auto",
+
+        )
+
+    def generate(self, prompt: str, temperature: float = 0.1, top_p: float = 1, max_tokens: int = 512):
+        messages =[{'role': 'system', 'content': self.system_message}, {'role': 'user', 'content': prompt}]
+        gen = self.model(messages, temperature=temperature, top_p=top_p, max_new_tokens=max_tokens)
+        if self.verbose:
+            print(gen)
+        self.history.append(gen)
+        return self.process_generation_to_code(gen)
+
+
+if __name__ == "__main__":
+
+    import re
+    from tqdm import tqdm
+    from datasets import load_dataset
+
+    gsm8k_test = load_dataset("openai/gsm8k", "main", split="test")
+    model_str = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+    model = LlamaProgramInterface(model=model_str)
+
+    all_score = []
+    i = 0
+    for sample in tqdm(gsm8k_test):
+
+        question = sample["question"]
+        answer = sample["answer"]
+        answer = answer.split("#### ")[-1]
+        answer = re.findall(r'\d+', answer)[0]
+        user_prompt = f"Write a function to solve the following problem, only write the program. The function must return `### value`:\n{question}"
+
+        output = model.generate(user_prompt)
+        print(output)
+        break
