97f0a668e4c2fca570d911115fa5152fb1ab3487
lintangsutawika <lintang@eleuther.ai>
2024-10-21 00:01:09 -0400

    fix logger

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
     logging.basicConfig(
         format="%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
         datefmt="%Y-%m-%d:%H:%M:%S",
-        level=logging.info,
+        level=logging.INFO,
     )
     logger = logging.getLogger(__name__)
 
