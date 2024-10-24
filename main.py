4da8e91abeeede5fb515de4b873572664f83c65c
lintangsutawika <lintang@eleuther.ai>
2024-10-24 00:28:39 -0400

    fix indentation

--- main.py
+++ main.py
@@ -140,10 +140,11 @@ if __name__ == "__main__":
 
     if args.inference_mode == "code":
         num_fewshot = 0
-        system_message = '''
-            Write a function to solve a given problem by the user. Only write the program. Do not use `print`.
-            The function must be named solution() and return `value` where value is only a number without any signs like '$' or '%'.
-            '''
+        system_message = """\
+Write a function to solve a given problem by the user. Only write the program. Do not use `print`.
+The function must be named solution() and return `value` where value is only a number without any signs like '$' or '%'.\
+"""
+
         model_system = HFProgramInterface(
             model=args.model_str,
             system_message=system_message,
@@ -155,10 +156,11 @@ if __name__ == "__main__":
         gsm8k_fewshot_output = None
     else:
         num_fewshot = args.num_fewshot
-        system_message = '''
-            Solve the problem by thinking step-by-step. Go through the reasoning in order to derive the final answer.
-            The final answer should follow the words 'So the answer is'.
-            '''
+        system_message = """\
+Solve the problem by thinking step-by-step. Go through the reasoning in order to derive the final answer.
+The final answer should follow the words 'So the answer is'.\
+"""
+
         model_system = HFNatLangInterface(
             model=args.model_str,
             system_message=system_message,
@@ -169,32 +171,31 @@ if __name__ == "__main__":
             )
 
         def gsm8k_fewshot_input(x):
-            fewshot_context = """
-            Question: There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?\n
-            Answer: There are 15 trees originally. Then there were 21 trees after some more were planted. So there must have been 21 - 15 = 6. The answer is 6.
+            fewshot_context = """\
+Question: There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?\n
+Answer: There are 15 trees originally. Then there were 21 trees after some more were planted. So there must have been 21 - 15 = 6. The answer is 6.
 
-            Question: If there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?
-            Answer: There are originally 3 cars. 2 more cars arrive. 3 + 2 = 5. The answer is 5.
+Question: If there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?
+Answer: There are originally 3 cars. 2 more cars arrive. 3 + 2 = 5. The answer is 5.
 
-            Question: Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?
-            Answer: Originally, Leah had 32 chocolates. Her sister had 42. So in total they had 32 + 42 = 74. After eating 35, they had 74 - 35 = 39. The answer is 39.
+Question: Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?
+Answer: Originally, Leah had 32 chocolates. Her sister had 42. So in total they had 32 + 42 = 74. After eating 35, they had 74 - 35 = 39. The answer is 39.
 
-            Question: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?
-            Answer: Jason started with 20 lollipops. Then he had 12 after giving some to Denny. So he gave Denny 20 - 12 = 8. The answer is 8.
+Question: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?
+Answer: Jason started with 20 lollipops. Then he had 12 after giving some to Denny. So he gave Denny 20 - 12 = 8. The answer is 8.
 
-            Question: Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?
-            Answer: Shawn started with 5 toys. If he got 2 toys each from his mom and dad, then that is 4 more toys. 5 + 4 = 9. The answer is 9.
+Question: Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?
+Answer: Shawn started with 5 toys. If he got 2 toys each from his mom and dad, then that is 4 more toys. 5 + 4 = 9. The answer is 9.
 
-            Question: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?
-            Answer: There were originally 9 computers. For each of 4 days, 5 more computers were added. So 5 * 4 = 20 computers were added. 9 + 20 is 29. The answer is 29.
+Question: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?
+Answer: There were originally 9 computers. For each of 4 days, 5 more computers were added. So 5 * 4 = 20 computers were added. 9 + 20 is 29. The answer is 29.
 
-            Question: Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?
-            Answer: Michael started with 58 golf balls. After losing 23 on tuesday, he had 58 - 23 = 35. After losing 2 more, he had 35 - 2 = 33 golf balls. The answer is 33.
+Question: Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?
+Answer: Michael started with 58 golf balls. After losing 23 on tuesday, he had 58 - 23 = 35. After losing 2 more, he had 35 - 2 = 33 golf balls. The answer is 33.
 
-            Question: Olivia has $23. She bought five bagels for $3 each. How much money does she have left?
-            Answer: Olivia had 23 dollars. 5 bagels for 3 dollars each will be 5 x 3 = 15 dollars. So she has 23 - 15 dollars left. 23 - 15 is 8. The answer is 8.
-
-            """
+Question: Olivia has $23. She bought five bagels for $3 each. How much money does she have left?
+Answer: Olivia had 23 dollars. 5 bagels for 3 dollars each will be 5 x 3 = 15 dollars. So she has 23 - 15 dollars left. 23 - 15 is 8. The answer is 8.\
+"""
             return fewshot_context
 
         def gsm8k_fewshot_output(x):
