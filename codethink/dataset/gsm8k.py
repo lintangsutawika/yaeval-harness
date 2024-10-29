bbba1c877c96908e053470363176aea1dae64597
lintang <lsutawik@andrew.cmu.edu>
2024-10-29 14:31:38 -0400

    add datasets

--- codethink/dataset/gsm8k.py
+++ codethink/dataset/gsm8k.py
@@ -2,6 +2,7 @@ import re
 from functools import partial
 
 from codethink.dataset.data import TransformedDataset
+# from data import TransformedDataset
 
 def gsm8k_fewshot_input(x):
     fewshot_context = """\
