d212e88a788889dc8f05ae3901569bfde86f75a9
lintang <lsutawik@andrew.cmu.edu>
2024-10-29 11:26:01 -0400

    add task arg

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datasets import load_dataset
 from codethink.utils import simple_parse_args_string
 # from codethink.interface import HFProgramInterface, HFNatLangInterface
 from codethink import INTERFACE
-from codethink.dataset import TransformedDataset
+from codethink.dataset import DATASET
 from codethink.evaluation import EvaluateSystem
 
 
@@ -98,6 +98,12 @@ def setup_parser() -> argparse.ArgumentParser:
         help="Number of seed",
     )
     parser.add_argument(
+        "--task",
+        default="gsm8k",
+        type=str,
+        help="Task to evaluate model on",
+    )
+    parser.add_argument(
         "--n_samples",
         default=None,
         type=int,
@@ -168,73 +174,20 @@ The function must be named solution() and return `value` where value is only a n
         num_fewshot = args.num_fewshot
         system_message = """\
 Solve the problem by thinking step-by-step. Go through the reasoning in order to derive the final answer.
-The final answer should follow the words 'So the answer is'.\
+At the end, you MUST write the answer as an integer after '####'."\
 """
+# The final answer should follow the words 'So the answer is'.\
+
 
         model_system = INTERFACE[args.inference_mode](
             model=args.model_str,
             system_message=system_message,
-            get_answer_symbol=[r"answer is (\-?[0-9\.\,]+)", r"answer is \$(\-?[0-9\.\,]+)"],
+            # get_answer_symbol=[r"answer is (\-?[0-9\.\,]+)", r"answer is \$(\-?[0-9\.\,]+)"],
             verbose=args.verbose,
             model_kwargs=simple_parse_args_string(args.model_kwargs)
             )
 
-        def gsm8k_fewshot_input(x):
-            fewshot_context = """\
-Question:\nThere are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?
-Answer:\nThere are 15 trees originally. Then there were 21 trees after some more were planted. So there must have been 21 - 15 = 6. The answer is 6.
-
-Question:\nIf there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?
-Answer:\nThere are originally 3 cars. 2 more cars arrive. 3 + 2 = 5. The answer is 5.
-
-Question:\nLeah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?
-Answer:\nOriginally, Leah had 32 chocolates. Her sister had 42. So in total they had 32 + 42 = 74. After eating 35, they had 74 - 35 = 39. The answer is 39.
-
-Question:\nJason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?
-Answer:\nJason started with 20 lollipops. Then he had 12 after giving some to Denny. So he gave Denny 20 - 12 = 8. The answer is 8.
-
-Question:\nShawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?
-Answer:\nShawn started with 5 toys. If he got 2 toys each from his mom and dad, then that is 4 more toys. 5 + 4 = 9. The answer is 9.
-
-Question:\nThere were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?
-Answer:\nThere were originally 9 computers. For each of 4 days, 5 more computers were added. So 5 * 4 = 20 computers were added. 9 + 20 is 29. The answer is 29.
-
-Question:\nMichael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?
-Answer:\nMichael started with 58 golf balls. After losing 23 on tuesday, he had 58 - 23 = 35. After losing 2 more, he had 35 - 2 = 33 golf balls. The answer is 33.
-
-Question:\nOlivia has $23. She bought five bagels for $3 each. How much money does she have left?
-Answer:\
-"""
-# Answer: Olivia had 23 dollars. 5 bagels for 3 dollars each will be 5 x 3 = 15 dollars. So she has 23 - 15 dollars left. 23 - 15 is 8. The answer is 8.\
-
-            return fewshot_context
-
-        # def gsm8k_fewshot_output(x):
-        #     answer = x["answer"]
-        #     answer = re.sub("####", "So the answer is", answer)
-        #     return answer
-
-        def gsm8k_fewshot_output(x):
-            return "Olivia had 23 dollars. 5 bagels for 3 dollars each will be 5 x 3 = 15 dollars. So she has 23 - 15 dollars left. 23 - 15 is 8. The answer is 8."
-
-    def gsm8k_input(x):
-        return "Question: " + x["question"] + "\nAnswer:"
-
-    def gsm8k_output(x):
-        answer = x["answer"]
-        answer = answer.split("#### ")[-1]
-        answer = float(re.findall(r'\d+', answer)[0])
-        return answer
-
-    gsm8k_dataset = TransformedDataset(
-        data_path="gsm8k",
-        data_name="main",
-        input_text=gsm8k_input,
-        output_text=gsm8k_output,
-        fewshot_input_text=gsm8k_fewshot_input,
-        fewshot_output_text=gsm8k_fewshot_output,
-        test_split="test",
-        fewshot_split="train",
+    eval_dataset = DATASET[args.task](
         num_fewshot=num_fewshot,
         sampler=None,
         n_samples=args.n_samples,
@@ -242,7 +195,7 @@ Answer:\
 
     evaluator = EvaluateSystem(
         model_system=model_system,
-        dataset=gsm8k_dataset,
+        dataset=eval_dataset,
         run_name=args.run_name,
         output_path=args.output_path,
     )
