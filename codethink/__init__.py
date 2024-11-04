26fe9839170bcc09e9ca51d13298373aea559180
lintang <lsutawik@andrew.cmu.edu>
2024-11-04 15:23:17 -0500

    move system mesasge

--- codethink/__init__.py
+++ codethink/__init__.py
@@ -7,3 +7,14 @@ INTERFACE = {
     "code": HFProgramInterface,
     "cot": HFNatLangInterface,
 }
+
+SYSTEM_MESSAGE = {
+    "code": """\
+Solve the problem by DIRECTLY and ONLY writing a program. The function must be named solution() without any input arguments.
+At the end, you MUST return an single value.\
+""",
+    "nl" : """\
+Solve the problem by thinking step-by-step. Go through the reasoning in order to derive the final answer.
+At the end, you MUST write the answer as an integer after '####'."\
+"""
+}
