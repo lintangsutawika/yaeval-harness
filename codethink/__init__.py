1c5f82bfc5514c2f43cada76cefec13ea1b7a9c1
lintang <lsutawik@andrew.cmu.edu>
2024-11-11 21:10:52 -0500

    adjustment

--- codethink/__init__.py
+++ codethink/__init__.py
@@ -13,7 +13,7 @@ SYSTEM_MESSAGE = {
 Solve the problem by DIRECTLY and ONLY writing a program. The function must be named solution() without any input arguments.
 At the end, you MUST return an single value.\
 """,
-    "nl" : """\
+    "cot" : """\
 Solve the problem by thinking step-by-step. Go through the reasoning in order to derive the final answer.
 At the end, you MUST write the answer as an integer after '####'."\
 """
