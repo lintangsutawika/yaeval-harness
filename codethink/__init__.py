bb749d338eee968f1f89e43ba300125137305d0e
lintang <lsutawik@andrew.cmu.edu>
2024-11-12 20:36:56 -0500

    add additional system messages

--- codethink/__init__.py
+++ codethink/__init__.py
@@ -13,6 +13,15 @@ SYSTEM_MESSAGE = {
 Solve the problem by DIRECTLY and ONLY writing a program. The function must be named solution() without any input arguments.
 At the end, you MUST return an single value.\
 """,
+    "code-comment": """\
+Solve the problem by DIRECTLY and ONLY writing a program. The function must be named solution() without any input arguments.
+Explain the reasoning in the program with comments.
+At the end, you MUST return an single value.\
+""",
+    "code-python": """\
+Solve the problem by DIRECTLY and ONLY writing a program with the PYTHON programming language. The function must be named solution() without any input arguments.
+At the end, you MUST return an single value.\
+""",
     "cot" : """\
 Solve the problem by thinking step-by-step. Go through the reasoning in order to derive the final answer.
 At the end, you MUST write the answer as an integer after '####'."\
