727db79a64091436216df50b6588394b83d7f165
lintang <lsutawik@andrew.cmu.edu>
2024-11-26 11:00:26 -0500

    adjust prompts

--- codethink/__init__.py
+++ codethink/__init__.py
@@ -72,13 +72,13 @@ Solve the problem by thinking step-by-step. Go through the reasoning in order to
 At the end, you MUST write the answer as an integer after '####'."\
 """,
     "routing-cot-first" : """\
-Choose between either Code or CoT as a way to solve a given task:
-1. CoT: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, you MUST write the answer after '####'.
-2. Code: DIRECTLY and ONLY writing a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, you MUST return an single value.\
+Choose only one way to solve the problem: by thinking step-by-step or writing a program as a way to solve a given task. Do NOT use both:
+1. Thinkng step-by-step: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, you MUST write the answer after '####'.
+2. Writing a program: DIRECTLY and ONLY write a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, you MUST return an single value.\
 """,
     "routing-code-first" : """\
-Choose between either Code or CoT as a way to solve a given task:
-1. Code: DIRECTLY and ONLY writing a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, you MUST return an single value.
-2. CoT: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, you MUST write the answer after '####'.\
+Choose only one way to solve the problem: by writing a program or thinking step-by-step as a way to solve a given task. Do NOT use both:
+1. Writing a program: DIRECTLY and ONLY write a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, you MUST return an single value.
+2. Thinkng step-by-step: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, you MUST write the answer after '####'.\
 """,
 }
