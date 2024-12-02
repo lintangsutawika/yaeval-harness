f87ac4218eb9d5e8d81fcb63579b013cfc8755f2
lintang <lsutawik@andrew.cmu.edu>
2024-12-02 00:09:32 -0500

    add use_run_name

--- codethink/evaluation.py
+++ codethink/evaluation.py
@@ -20,6 +20,7 @@ class EvaluateSystem:
                  run_name=None,
                  output_path=None,
                  run_args=None,
+                 use_run_name=True,
                  batch_size=1,
                  **kwargs
                  ):
@@ -32,12 +33,12 @@ class EvaluateSystem:
         else:
             self.run_name = run_name
         self.output_path = output_path
+        self.use_run_name = use_run_name
         self.run_args = run_args
 
     def run(self, temperature=0.1, top_p=1.0, repeat=1, seed=None):
         result_dict = {
             "n_samples": 0,
-            "score": 0,
             "duration": 0,
             "total_tokens": 0,
             "input_tokens": 0,
@@ -63,12 +64,18 @@ class EvaluateSystem:
             ans_list, output_dict_list = self.model_system.run(user_input, temperature=temperature, top_p=top_p, repeat=repeat, seed=seed)
 
             for ans, gt, inp, output_dict in zip(ans_list, ground_truth, user_input, output_dict_list):
-                score = self.dataset.eval(ans, gt)
+                score_dict = self.dataset.eval(ans, gt)
+                score_string = ", ".join([f"{metric}: {score}" for metric,score in score_dict.items()])
 
                 # if self.verbose:
-                logger.info(f"\nId: {idx}, Score: {score}, Prediction: {ans}, Ground Truth: {gt}")
+                logger.info(f"\nId: {idx}, {score_string}, Prediction: {ans}, Ground Truth: {gt}")
                 result_dict["n_samples"] += 1
-                result_dict["score"] += score
+                for metric, score in score_dict.items():
+                    if metric not in result_dict:
+                        result_dict[metric] = score
+                    else:
+                        result_dict[metric] += score
+
                 result_dict["duration"] += output_dict["duration"]
                 result_dict["input_tokens"] += output_dict["input_len"]
                 result_dict["output_tokens"] += output_dict["output_len"]
@@ -76,7 +83,7 @@ class EvaluateSystem:
                 output_json.append(
                     {
                         "idx": idx,
-                        "score": score,
+                        **score_dict,
                         "ground_truth": gt,
                         "answer": ans,
                         "user_input": inp,
@@ -84,18 +91,97 @@ class EvaluateSystem:
                     }
                 )
 
-                data_dict["idx"].append(int(idx))
-                data_dict["answer"].append(ans)
-                data_dict["system_output"].append(output_dict["system_output"][0])
-                data_dict["ground_truth"].append(gt)
-                data_dict["user_input"].append(user_input)
-                data_dict["score"].append(score)
-                data_dict["duration"].append(output_dict["duration"])
-                data_dict["input_tokens"].append(output_dict["input_len"])
-                data_dict["output_tokens"].append(output_dict["output_len"])
-                data_dict["total_tokens"].append(output_dict["input_len"] + output_dict["output_len"])
+                # data_dict["idx"].append(int(idx))
+                # data_dict["answer"].append(ans)
+                # data_dict["system_output"].append(output_dict["system_output"])
+                # data_dict["ground_truth"].append(gt)
+                # data_dict["user_input"].append(user_input)
+                # data_dict["score"].append(score)
+                # data_dict["duration"].append(output_dict["duration"])
+                # data_dict["input_tokens"].append(output_dict["input_len"])
+                # data_dict["output_tokens"].append(output_dict["output_len"])
+                # data_dict["total_tokens"].append(output_dict["input_len"] + output_dict["output_len"])
                 idx += 1
             # zeno_upload(self.run_name, data_dict)
+
+        result_dict = {**self.run_args, **result_dict}
+        for metric, score in score_dict.items():
+            result_dict[f"avg_{metric}"] = result_dict[metric]/result_dict["n_samples"]
+        result_dict["avg_duration"] = result_dict["duration"]/result_dict["n_samples"]
+        result_dict["avg_input_tokens"] = result_dict["input_tokens"]/result_dict["n_samples"]
+        result_dict["avg_output_tokens"] = result_dict["output_tokens"]/result_dict["n_samples"]
+        result_dict["avg_total_tokens"] = result_dict["total_tokens"]/result_dict["n_samples"]
+        logger.info(f"{self.run_name} complete")
+        logger.info(
+            ",".join(
+                [f"{metric}: {result_dict[f'avg_{metric}']}" for metric in score_dict.keys()]
+                )
+            )
+
+        if self.output_path is not None:
+            if self.use_run_name:
+                run_path = os.path.join(self.output_path, self.run_name)
+            else:
+                run_path = os.path.join(self.output_path)
+            os.makedirs(run_path, exist_ok=True)
+            result_file = os.path.join(run_path, "result.json")
+            with open(result_file, 'w', encoding='utf-8') as file:
+                json.dump(result_dict, file, ensure_ascii=False, indent=4)
+
+            try:
+                output_file = os.path.join(run_path, "output.jsonl")
+                with jsonlines.open(output_file, "w") as file:
+                    file.write_all(output_json)
+            except Exception as e:
+                print(e)
+
+        return 0
+
+class OracleEvaluation(EvaluateSystem):
+
+    def run(self, temperature=0.1, top_p=1.0, repeat=1, seed=None):
+
+        result_dict = {
+            "n_samples": 0,
+            "score": 0,
+            "duration": 0,
+            "total_tokens": 0,
+            "input_tokens": 0,
+            "output_tokens": 0,
+        }
+        output_json = []
+        idx = 0
+
+        while True:
+            for sample in tqdm(self.data_loader):
+                user_input, ground_truth = sample
+
+                ans_list, output_dict_list = self.model_system.run(user_input, temperature=temperature, top_p=top_p, repeat=repeat, seed=seed)
+
+                for ans, gt, inp, output_dict in zip(ans_list, ground_truth, user_input, output_dict_list):
+                    score = self.dataset.eval(ans, gt)
+
+                    if float(score) != 1.0:
+                        ans, gt, inp, 
+
+                    # if self.verbose:
+                    logger.info(f"\nId: {idx}, Score: {score}, Prediction: {ans}, Ground Truth: {gt}")
+                    result_dict["n_samples"] += 1
+                    result_dict["score"] += score
+                    result_dict["duration"] += output_dict["duration"]
+                    result_dict["input_tokens"] += output_dict["input_len"]
+                    result_dict["output_tokens"] += output_dict["output_len"]
+                    result_dict["total_tokens"] += (output_dict["input_len"] + output_dict["output_len"])
+                    output_json.append(
+                        {
+                            "idx": idx,
+                            "score": score,
+                            "ground_truth": gt,
+                            "answer": ans,
+                            "user_input": inp,
+                            **output_dict,
+                        }
+                    )
 
         result_dict = {**self.run_args, **result_dict}
         result_dict["avg_score"] = result_dict["score"]/result_dict["n_samples"]
