f9bc7f11b69e48ae0a14b547f6ae001b0cb48b1d
lintang <lsutawik@andrew.cmu.edu>
2024-12-11 17:17:07 -0500

    removed comments

--- codethink/__init__.py
+++ codethink/__init__.py
@@ -1,18 +1,13 @@
-# from vllm import ModelRegistry
-# from .modeling import AutoModelforCodeSolverLM
-# ModelRegistry.register_model("AutoModelforCodeSolverLM", AutoModelforCodeSolverLM)
-
 from .interface import (
-    # HFProgramInterface,
     SolverInterface,
 )
 
 INTERFACE = {
-    # "code": HFProgramInterface,
     "default": SolverInterface,
 }
 
 SYSTEM_MESSAGE = {
+    "none": "",
     "code": """\
 Solve the problem by DIRECTLY and ONLY writing a program. The function must be named solution() without any input arguments.
 At the end, you MUST return an single value.\
@@ -72,12 +67,12 @@ Solve the problem by thinking step-by-step. Go through the reasoning in order to
 At the end, you MUST write the answer as an integer after '####'."\
 """,
     "routing-cot-first" : """\
-Choose only one way to solve the problem: by thinking step-by-step or writing a program as a way to solve a given task. Do NOT use both:
+Choose only one way to solve the problem: by thinking step-by-step OR writing a program as a way to solve a given task. Do NOT use both:
 1. Thinkng step-by-step: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, you MUST write the answer after '####'.
 2. Writing a program: DIRECTLY and ONLY write a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, you MUST return an single value.\
 """,
     "routing-code-first" : """\
-Choose only one way to solve the problem: by writing a program or thinking step-by-step as a way to solve a given task. Do NOT use both:
+Choose only one way to solve the problem: by writing a program OR thinking step-by-step as a way to solve a given task. Do NOT use both:
 1. Writing a program: DIRECTLY and ONLY write a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, you MUST return an single value.
 2. Thinkng step-by-step: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, you MUST write the answer after '####'.\
 """,
