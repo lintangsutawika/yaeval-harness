1f32a7b7edb33fd9f2768db8845d43478941b56f
lintang <lsutawik@andrew.cmu.edu>
2025-01-27 18:38:37 -0500

    adjust task call

--- codethink/dataset/__init__.py
+++ codethink/dataset/__init__.py
@@ -1,42 +1,31 @@
-from .data import TransformedDataset
+import os
+import glob
+import importlib
 
-from .svamp import SVAMPDataset
-from .gsm8k import GSM8KDataset, GSM8KRoutingDataset
-from .aqua import AQUADataset
-from .multiarith import MultiArithDataset
-from .tabmwp import TabMWPDataset
-from .bbh import BBHDataset
-from .gsm_hard import GSMHardDataset, GSMHardGenerateTestsDataset
-from .finqa import FinQADataset
-from .coinflip import CoinFlipDataset
-from .algebra import AlgebraDataset
-from .arc_challenge import ARCDataset
-from .lastletterconcat import LastLetterConcatDataset
-from .mathqa import MathQADataset
-from .aime import AIMEDataset
-from .hendrycks_math import mathdatasets
-from .commonsense_qa import CommonsenseQADataset, CommonsenseQARoutingDataset
-from .num_glue import num_glue_tasks
+from functools import partial
+# from .data import TransformedDataset
 
-DATASET = {
-    "svamp": SVAMPDataset,
-    "gsm8k": GSM8KDataset,
-    "aqua": AQUADataset,
-    "multiarith": MultiArithDataset,
-    "tabmwp": TabMWPDataset,
-    **BBHDataset,
-    "gsmhard": GSMHardDataset,
-    "gsmhard_generate_tests": GSMHardGenerateTestsDataset,
-    "finqa": FinQADataset,
-    "mathqa": MathQADataset,
-    "coinflip": CoinFlipDataset,
-    "lastletterconcat": LastLetterConcatDataset,
-    "algebra": AlgebraDataset,
-    "arc_challenge": ARCDataset,
-    "aime": AIMEDataset,
-    "commonsense_qa": CommonsenseQADataset,
-    **mathdatasets,
-    **num_glue_tasks,
-    "routing_gsm8k": GSM8KRoutingDataset,
-    "routing_commonsense_qa": CommonsenseQARoutingDataset,
-}
+TASK_LIST = {}
+
+# Decorator to register functions
+def register_task(name):
+    def decorator(obj):
+        obj = partial(obj, name=name)
+        TASK_LIST[name] = obj
+        globals()[name] = obj
+        return obj
+    return decorator
+
+module_files = glob.glob(
+    os.path.join(
+        os.path.dirname(__file__), "*.py"
+        )
+    )
+
+for file in module_files:
+    module_name = os.path.basename(file)[:-3]
+    if module_name != "__init__" and module_name.isidentifier():
+        importlib.import_module(f".{module_name}", package=__name__)
+
+__all__ = list(TASK_LIST.keys())
+
