614c84eee514769d91a2ce805e8796641837111f
lintang <lsutawik@andrew.cmu.edu>
2025-02-02 12:23:14 -0500

    add num_glue

--- codethink/dataset/num_glue/num_glue.py
+++ codethink/dataset/num_glue/num_glue.py
@@ -1,10 +1,12 @@
 import os
 from functools import partial
 
-try:
-    from codethink.dataset.data import TransformedDataset
-except:
-    from data import TransformedDataset
+from codethink.dataset import register_task
+
+from codethink._task import Task, create_task
+from codethink._data import TransformedDataset
+
+from codethink.utils import is_runnable_code
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -51,20 +53,63 @@ def num_glue_eval(prediction, ground_truth):
 
     return score
 
-num_glue_tasks = {}
 for num in range(1,9):
-    num_glue_tasks[f"numglue_type_{num}"] = partial(
+    num_glue_tasks[f'type-{num}'] = partial(
         TransformedDataset,
         data_path="json",
         data_name={
             "test": os.path.join(dir_path, f"NumGLUE-Type_{num}.jsonl"),
-            },
+        },
         input_text=num_glue_input,
         output_text=num_glue_output,
         evaluation=num_glue_eval,
         test_split="test",
         fewshot_split="test",
     )
+
+def preprocess_routing(x, state):
+    current_step = state["current_step"]
+    solve_with = state["step"][current_step-1]["output"]
+    if solve_with == "programming language":
+        state["system_message"] = "code"
+    elif solve_with == "natural language":
+        state["system_message"] = "cot"
+    return x, state
+
+def postprocess_routing(x, state):
+    x = x["response"][0]
+    current_step = state["current_step"]
+    solve_with = state["step"][current_step-1]["output"]
+    if solve_with == "programming language":
+        x = is_runnable_code(x) 
+    elif solve_with == "natural language":
+        try:
+            x = x.split("answer is")[-1].strip()
+        except:
+            pass
+    return x, state
+
+@register_task(
+    "numglue_type_1_routing_pl_first",
+    subtask_list=[
+        Task(
+            name="numglue_type_1_routing",
+            dataset=partial(
+                num_glue_tasks["type-1"],
+                input_text=lambda x: num_glue_input(x).replace("\nAnswer:", "")+"\n\nWhich method is the best way to solve this problem?",
+            ),
+            system_message="routing_selection_pl_first",
+        ),
+        Task(
+            name="numglue_type_1",
+            dataset=num_glue_tasks["type-1"],
+            preprocess=preprocess_routing,
+            postprocess=postprocess_routing,
+            evaluation={"accuracy": num_glue_eval},
+        ),
+    ])
+class NumGLUEType1(Task):
+    pass
 
 if __name__ == "__main__":
 
