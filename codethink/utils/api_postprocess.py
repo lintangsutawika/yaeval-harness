87b70f6a2d3076404f13664540b536cfeb3757e5
lintang <lintang@sutawika.com>
2025-03-22 21:43:26 -0400

    add utils here

---
+++ codethink/utils/api_postprocess.py
@@ -0,0 +1,58 @@
+import ast
+
+"""
+Example response from OpenAI Chat Completion
+{
+  "id": "chatcmpl-B9MBs8CjcvOU2jLn4n570S5qMJKcT",
+  "object": "chat.completion",
+  "created": 1741569952,
+  "model": "gpt-4o-2024-08-06",
+  "choices": [
+    {
+      "index": 0,
+      "message": {
+        "role": "assistant",
+        "content": "Hello! How can I assist you today?",
+        "refusal": null,
+        "annotations": []
+      },
+      "logprobs": null,
+      "finish_reason": "stop"
+    }
+  ],
+  "usage": {
+    "prompt_tokens": 19,
+    "completion_tokens": 10,
+    "total_tokens": 29,
+    "prompt_tokens_details": {
+      "cached_tokens": 0,
+      "audio_tokens": 0
+    },
+    "completion_tokens_details": {
+      "reasoning_tokens": 0,
+      "audio_tokens": 0,
+      "accepted_prediction_tokens": 0,
+      "rejected_prediction_tokens": 0
+    }
+  },
+  "service_tier": "default"
+}
+"""
+def vllm_postprocess(response):
+    """
+    Returns list of responses and the original response object
+    """
+    response_dict = ast.literal_eval(
+        response.model_dump_json(
+            exclude_unset=True,
+            exclude_none=True
+            )
+        )
+    return (
+        [
+            response.choices[i].message.content
+            for i in range(0, len(response.choices))
+        ],
+        response_dict
+    )
+
--- codethink/utils/general.py
+++ codethink/utils/general.py
@@ -81,35 +81,6 @@ def zeno_upload(run_name, result_dict, metric_column="score"):
     project.upload_dataset(df, id_column="idx", data_column="user_input", label_column="answer")
     project.upload_system(df[["idx", "system_output"]], name="System A", id_column="idx", output_column="system_output")
 
-def process_generation_to_code(gens: str, answer_expr: str):
-    if '```python' in gens:
-        gens = gens.split('```python')[1].split('```')[0]
-    elif '```' in gens:
-        gens = gens.split('```')[1].split('```')[0]
-    elif answer_expr in gens:
-        gens = "def "+answer_expr+f"{answer_expr}".join(gens.split(answer_expr)[1:])
-    else:
-        return False
-        
-    return gens.split('\n')
-
-def is_runnable_code(text_string, answer_expr='solution()', time_out=10):
-    # Check if the _output is a program
-    code = process_generation_to_code(text_string, answer_expr)
-    if code:
-        def _generate_code(code, answer_expr):
-            return "\n".join(code)+f"\nans = 'ans='+str({answer_expr})\nprint(ans)"
-        # Generate code snippet that will be executed in a different process
-        code_snippet = _generate_code(code, answer_expr)
-        try:
-            subprocess_result = subprocess.run([sys.executable, "-c", code_snippet], timeout=time_out, text=True, capture_output=True)
-            exec_result = subprocess_result.stdout.split("ans=")[-1].strip()
-            return exec_result
-        except Exception as e:
-            return False
-    else:
-        return False
-
 def check_api_health(url):
     try:
         response = requests.get(url)
