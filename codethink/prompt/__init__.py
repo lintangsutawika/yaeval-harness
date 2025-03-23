79d2b0f81fc5a95626bbac39771862239d4e4ed3
lintang <lintang@sutawika.com>
2025-03-23 00:16:03 -0400

    add prompts

---
+++ codethink/prompt/__init__.py
@@ -0,0 +1,72 @@
+import os
+import glob
+import importlib
+from functools import partial
+
+from .base_prompt import YevalPrompt
+from codethink.response import get_postprocess_fn
+
+PROMPT_LIST = {}
+
+def get_message_str(system_message):
+    if system_message in PROMPT_LIST:
+        system_message = PROMPT_LIST[system_message]
+
+    if isinstance(system_message, str):
+        return system_message
+    else:
+        if not issubclass(system_message, YevalPrompt):
+            raise TypeError(f"Expected a YevalPrompt subclass, got {type(system_message)}")
+        else:
+            return system_message.system_message
+
+def get_prompt(prompt):
+    if prompt is None:
+        return None, None
+    postprocessor = None
+    if prompt in PROMPT_LIST:
+        prompt = PROMPT_LIST[prompt]
+
+    if issubclass(prompt, YevalPrompt):
+        system_message = getattr(prompt, "system_message")
+        postprocessor = getattr(prompt, "postprocessor")
+    else:
+        system_message = system_message
+        postprocessor = None
+
+    if postprocessor is not None:
+        postprocessor = get_postprocess_fn(postprocessor)
+    return system_message, postprocessor
+
+
+# Decorator to register functions
+def register_prompt(name):
+    def decorator(obj):
+        PROMPT_LIST[name] = obj
+        globals()[name] = obj
+        return obj
+    return decorator
+
+def import_modules(path=None):
+
+    if path is None:
+        path = os.path.dirname(__file__)
+
+    module_files = glob.glob(
+        os.path.join(
+            path, "**", "*.py"
+            ), recursive=True
+        )
+
+    for file in module_files:
+        module_name = os.path.basename(file)[:-3]
+        if module_name != "__init__" and module_name.isidentifier():
+            spec = importlib.util.spec_from_file_location(f"{module_name}", file)
+            foo = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(foo)
+            # importlib.import_module(f".{module_name}", package=__name__)
+
+import_modules()
+
+__all__ = list(PROMPT_LIST.keys())
+
---
+++ codethink/prompt/base_prompt.py
@@ -0,0 +1,53 @@
+from typing import Callable, Optional
+
+class YevalPrompt:
+    system_message: str
+    postprocessor: callable=None
+
+# SYSTEM_MESSAGE = {
+#     "none": "",
+#     "code_old": """\
+# Solve with the following approach: DIRECTLY and ONLY write a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, you MUST return an single value.\
+# """,
+#     "code": """\
+# Solve the following problem by DIRECTLY and ONLY writing a PYTHON program. The answer mush be a function named solution() without any input arguments. The function MUST return an single value.\
+# """,
+#     "cot_old" : """\
+# Solve with the following approach: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, you MUST write the answer after 'The answer is'.
+# """,
+#     "cot": """\
+# Solve the following problem by thinking step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, you MUST write the answer after 'The answer is'.\
+# """,
+#     "routing_nl_first" : """\
+# Choose only one way to solve the problem: by thinking step-by-step OR writing a program as a way to solve a given task. Do NOT use both:
+# 1. Thinking step-by-step: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, you MUST write the answer after 'The answer is'.
+# 2. Writing a program: DIRECTLY and ONLY write a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, you MUST return an single value.\
+# """,
+#     "routing_pl_first" : """\
+# Choose only one way to solve the problem: by writing a program OR thinking step-by-step as a way to solve a given task. Do NOT use both:
+# 1. Writing a program: DIRECTLY and ONLY write a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, you MUST return an single value.
+# 2. Thinking step-by-step: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, you MUST write the answer after 'The answer is'.\
+# """,
+#     "routing_selection_answer_nl_first" : """\
+# Based on a given task, choose only one way that can be used to solve the problem: by natural language OR programming language to solve a given task. Do NOT use both:
+# 1. Natural Language: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, if this is the chosen method, answer "natural language".
+# 2. Programming Language: DIRECTLY and ONLY write a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, if this is the chosen method, answer "programming language".\
+# """,
+#     "routing_selection_answer_pl_first" : """\
+# Based on a given task, choose only one way that can be used to solve the problem: by natural language OR programming language to solve a given task. Do NOT use both:
+# 1. Programming Language: DIRECTLY and ONLY write a program with the PYTHON programming language. The function must be named solution() without any input arguments. At the end, if this is the chosen method, answer "programming language".\
+# 2. Natural Language: Think step-by-step. Derive and go through the logical steps in order to arrive at the final answer. At the end, if this is the chosen method, answer "natural language".
+# """,
+#     "routing_selection_nl_first" : """\
+# Based on a given task, choose only one way that can be used to solve the problem: by natural language OR programming language to solve a given task. Do NOT use both. Answer with either "natural language" or "programming language".
+# """,
+#     "routing_selection_pl_first" : """\
+# Based on a given task, choose only one way that can be used to solve the problem: by programming language OR natural language to solve a given task. Do NOT use both. Answer with either "programming language" or "natural language".
+# """,
+#           "math_reason" : "Reason step by step and put your final answer within \\boxed{}.",
+#     "eng_reason_in_ind" : "Reason step by step in Indonesian and put your final answer within \\boxed{}.",
+#     "ind_reason"        : "Berpikir langkah demi langkah dan tuliskan jawaban akhir di dalam \\boxed{}.",
+# "eng_reason_analogy_ind": "Use step-by-step reasoning and analogies to explain how to solve the problem using Indonesian. Put your final answer within \\boxed{}.",
+#     "eng_reason_in_jpn" : "Reason step by step in Japanese and put your final answer within \\boxed{}.",
+#     "jpn_reason"        : "段階的に理論を展開し、最終的な答えを \\boxed{} の中に入れてください。",
+# }
