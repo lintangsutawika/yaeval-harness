01b647686d3af32b58a3a1a718a04e6559c4fcdb
lintang <lintang@sutawika.com>
2025-03-26 10:30:06 -0400

    remove

--- preference_tuning/dataset/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._dataset import dpo_dataset
-from .mathinstruct_preference import MathInstructPoTDataset, MathInstructCoTDataset
-
-DATASET = {
-    "mathinstruct_pot": MathInstructPoTDataset,
-    "mathinstruct_cot": MathInstructCoTDataset,
-}
