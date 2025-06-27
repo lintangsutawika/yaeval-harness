b3864251ffde69f137ad26d98c7b285b9c4323db
lintang <lintang@sutawika.com>
2025-06-27 16:02:24 -0400

    average logprob

--- yeval/log/usage.py
+++ yeval/log/usage.py
@@ -1,16 +1,24 @@
 import numpy as np
 
 def log_token_usage(state):
-    input_tokens = state["usage"]["prompt_tokens"]
-    output_tokens = state["usage"]["completion_tokens"]
-    total_tokens = input_tokens + output_tokens
-    return {
-        "input_tokens": input_tokens,
-        "output_tokens": output_tokens,
-        "total_tokens": total_tokens,
-    }
+    try:
+        input_tokens = state["usage"]["prompt_tokens"]
+        output_tokens = state["usage"]["completion_tokens"]
+        total_tokens = input_tokens + output_tokens
+        return {
+            "input_tokens": input_tokens,
+            "output_tokens": output_tokens,
+            "total_tokens": total_tokens,
+        }
+    except KeyError:
+        return {}
 
 def log_logprob(state):
-    for choice in state['choices']:
-        choice_logprob = sum([token['logprob'] for token in choice['logprobs']['content']])
+    try:
+        choice_logprob = []
+        for choice in state['choices']:
+            logprob_list = [token['logprob'] for token in choice['logprobs']['content']]
+            choice_logprob.append(sum(logprob_list)/len(logprob_list))
         return {"logprob": choice_logprob}
+    except KeyError:
+        return {}
